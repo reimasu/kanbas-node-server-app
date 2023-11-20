719913e62b2511510a583c4c101723eae3f25678
reimasu <rei.masuya@gmail.com>
2023-11-20 15:25:23 -0500

    undo

--- courses/routes.js
+++ courses/routes.js
@@ -17,12 +17,12 @@ function CourseRoutes(app) {
   
     app.delete("/api/courses/:id", (req, res) => {
       const { id } = req.params;
-      const course = Database.courses.find((c) => c._id.$oid === id);
+      const course = Database.courses.find((c) => c._id === id);
       if (!course) {
         res.sendStatus(404);
         return;
       }
-      Database.courses = Database.courses.filter((c) => c._id. !== id);
+      Database.courses = Database.courses.filter((c) => c._id !== id);
       res.sendStatus(204);
     });
   
@@ -36,7 +36,7 @@ function CourseRoutes(app) {
       const { id } = req.params;
       const course = req.body;
       Database.courses = Database.courses.map((c) =>
-        c._id.$oid === id ? { c, ...course } : c
+        c._id === id ? { c, ...course } : c
       );
       res.sendStatus(204);
     });
