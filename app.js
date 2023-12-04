7635e27e26468e9c31ff48f392c4aab7a68cb89d
reimasu <rei.masuya@gmail.com>
2023-12-04 12:30:41 -0500

    he

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 import express from 'express';
+import session from "express-session";
+import "dotenv/config";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentsRoutes from './assignments/routes.js';
+import UserRoutes from "./users/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 import "dotenv/config";
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }
+ ));
+ const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentsRoutes(app);
+UserRoutes(app);
 Lab5(app);
 Hello(app)
 app.listen(process.env.PORT || 4000);
